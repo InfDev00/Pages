6983a40904c0156949dd20f43d52dfae1efd517a
InfDev00 <119485240+InfDev00@users.noreply.github.com>
2025-10-26 05:08:07 +0000

    update path

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          {count} 번 클릭했음.
+          {count} 번 클릭했음 zzz.
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
